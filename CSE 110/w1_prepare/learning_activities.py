afe5f90d450e61f49e9e49d4637ab55cebd41201
Boyd Querubin <boydisaacq@gmail.com>
2024-09-18 07:53:56 -0600

    first commit

---
+++ CSE 110/w1_prepare/learning_activities.py
@@ -0,0 +1,7 @@
+fav_color = input("Please type your favorite color: ").title()
+print(f"Your favorite color is:\n{fav_color}")
+
+first_name = input("What is your first name? ").title()
+last_name = input("What is your last name? ").title()
+
+print(f"Your name is {last_name}, {first_name} {last_name}")
